09a7afecb053de4d8a6a9cb06806fb8cb64c1783
Francisco Enriquez <franckenriquez63@gmail.com>
2022-01-01 18:26:08 -0600

    (feat/metodos) (autor/Francisco Enriquez)
    First methods on C#.

---
+++ beginner/Clases/Rectangulo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    class Rectangulo
+    {
+        // propiedades públicas
+        // Deben iniciar con letras mayúsculas
+        // Atributos
+        public int  Base { get; set; }
+        public int Altura { get; set; }
+
+        // Métodos
+        public int CalcularArea()
+        {
+            return Base * Altura;
+        }
+
+        // Esta clase tiene 2 atributos/propiedades y 1 método
+    }
+}
---
+++ beginner/ControlAcceso/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AnotherNameSpace;
+
+namespace ControlAcceso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.MetodoPublico();
+            obj.MetodoInternal();
+            // el método protected y private no se pueden llamar
+            // obj.MetodoProtected();
+            // obj.MetodoPrivado();
+        }
+    }
+}
